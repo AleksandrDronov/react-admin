7770aa3b0d9514a3b268a60b29169d322b550865
Aleksandr Dronov <dronovspb@gmail.com>
2023-05-02 16:25:46 +0300

    form

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -104,8 +104,8 @@ const SidebarMenu = () => {
                   />
                 </Box>
                 <Box textAlign="center">
-                  <Typography variant="h2" color={colors.grey[100]} fontWeight="bold" sx={{ m: "10px 0 0 0"}}>Ed Roh</Typography>
-                  <Typography variant="h5" color={colors.greenAccent[500]}>VP Fancy Admin</Typography>
+                  <Typography variant="h2" color={colors.grey[100]} fontWeight="bold" sx={{ m: "10px 0 0 0"}}>Dronov</Typography>
+                  <Typography variant="h5" color={colors.greenAccent[500]}>Admin</Typography>
                 </Box>
               </Box>
             )}
